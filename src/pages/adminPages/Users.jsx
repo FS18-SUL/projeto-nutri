32845ad7e13e1aea753fb99f08bbd7248ea69a10
Francisco Gleidson Teixeira Rocha <Francisco.Rocha@digitalcollege.internal>
2025-03-12 12:09:15 -0300

    updates

--- src/pages/adminPages/Users.jsx
+++ src/pages/adminPages/Users.jsx
@@ -1,12 +1,43 @@
-import { Button, Drawer } from "antd";
-import { useState } from "react";
+import { Button, Drawer, Form, Input, message } from "antd";
+import { useEffect, useState } from "react";
+import { API } from "../../services";
 
 const Users = () => {
 
     const [visibleCreate, setVisibleCreate] = useState(false);
-    
+    const [formCreate] = Form.useForm();
+
+    const [messageApi, contextHolder] = message.useMessage();
+    const warning = (mensagem) => {
+        messageApi.warning(mensagem);
+    };
+    const success = (mensagem) => {
+        messageApi.success(mensagem);
+    };
+
+    async function listarUsuarios() {
+        const request = await API.get("/usuarios");
+        const response = await request;
+        console.log(response.data);
+    }
+
+    async function criarUsuario(dados) {
+        const request = await API.post("/usuarios", dados);
+        const response = await request;
+
+        if (response.status == 201) {
+            setVisibleCreate(false);
+            success(`Usuário ${response.statusText}`);
+        }
+    }
+
+    useEffect(() => {
+        listarUsuarios();
+    }, [])
+
     return (
         <>
+            {contextHolder}
             <h1 className="flex justify-between items-center">
                 usuários
                 <Button
@@ -17,14 +48,64 @@ const Users = () => {
                 </Button>
             </h1>
 
-
+            {/* A Table vem aqui */}
+            
 
             <Drawer
                 open={visibleCreate}
                 onClose={() => setVisibleCreate(false)}
                 title="Cadastrar"
             >
-                alguma coisa
+                <Form
+                    layout="vertical"
+                    form={formCreate}
+                    onFinish={criarUsuario}
+                >
+                    <Form.Item
+                        name="usuario_nome"
+                        label="Nome"
+                        rules={[
+                            {
+                                required: true,
+                                message: "O nome é obrigatório!"
+                            },
+                        ]}
+                    >
+                        <Input />
+                    </Form.Item>
+                    <Form.Item
+                        name="usuario_email"
+                        label="Email"
+                        rules={[
+                            {
+                                required: true,
+                                message: "O email é obrigatório!"
+                            },
+                        ]}
+                    >
+                        <Input type="email" />
+                    </Form.Item>
+                    <Form.Item
+                        name="usuario_senha"
+                        label="Senha"
+                        rules={[
+                            {
+                                required: true,
+                                message: "A senha é obrigatória!"
+                            },
+                        ]}
+                    >
+                        <Input.Password />
+                    </Form.Item>
+
+                    <Button
+                        type="primary"
+                        htmlType="submit"
+                    >
+                        Criar
+                    </Button>
+                </Form>
+
             </Drawer>
         </>
     );
